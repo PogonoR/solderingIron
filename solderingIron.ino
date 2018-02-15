75b34e999f2e31f66f43f447faa3d1085482fb27
PogonoR <PogonoR@users.noreply.github.com>
2018-02-15 21:00:05 +0100

    v0.6 functional oled
    completed oled display support to display currently set tmperature, power level an temperature overshoot bar graphs

--- solderingIron.ino
+++ solderingIron.ino
@@ -1,7 +1,9 @@
 //*********************** header *******************************
-#define version "v0.5"
-#define date "2018-02-14"
+#define version "v0.6"
+#define date "2018-02-15"
 /* changelog:
+ *v.06 (2018-02-15)
+  -OLED now functional, showing set temperature, power and temp_overshoot bar graphs
   v.05 (2018-02-14)
   -added OLED screen support
   v0.4 (2018-02-13)
@@ -22,8 +24,8 @@ boolean heating = 0;
 ResponsiveAnalogRead analog_set(pot_in, true);               // create input setting reading object
 
 /************** measure ******************/
-#define temp_read_pin A5                                     // pin for reading temperature
-#define current_read_pin A4                                  // pin for checking current
+#define temp_read_pin A3                                     // pin for reading temperature
+#define current_read_pin A2                                  // pin for checking current
 ResponsiveAnalogRead analog_temp(temp_read_pin, true);       // create temperature reading object
 ResponsiveAnalogRead analog_current(current_read_pin, true); // createcurrent reading object
 unsigned long tick = 0;                                      // placeholder to store time millis snapshots
@@ -35,8 +37,9 @@ float current;                                               // placeholder to h
 /************** display ******************/
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define OLED_RESET 4
+#define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_RESET);
+char buffor[3];
 
 
 
@@ -67,11 +70,9 @@ void setup() {
 /************************************************************************************************/
 void loop() {
   /************** measure *****************/
-  //int temp=analogRead(temp_read_pin);
-
   if (millis() - tick > 50) {                                // filter this operation every (XX) often
     analogWrite(heating_pin, 0);                             // turn off heating
-    delay(5);                                                // wait 5 millis for currents to drop
+    delay(1);                                                // wait 5 millis for currents to drop
     analog_temp.update();                                    // perform temperature measurement
     temp_read = analog_temp.getValue();                      // pull in filtered temperature reading
     temp = map(temp_read, 0, 1023, 20, 450);                 // decode temp reading to temperature (C)
@@ -88,7 +89,7 @@ void loop() {
   int val = analog_set.getValue();                               // pull in filtered inpu setting reading
   float perc = val * 10;                                     // translate RAW controller input into % PWR
   perc = perc / 102;
-  int temp_set = map(val, 0, 1023, 0, 450);                  // translated input to temperature (C)
+  int temp_set = map(val, 0, 1023, 20, 450);                  // translated input to temperature (C)
   /************** heating ******************/
   if ( temp < (temp_set - temp_window)) {                    // when tip temp is way to low, 
     val = 1023;
@@ -108,33 +109,40 @@ void loop() {
   Serial.print("I: "); Serial.print(current); Serial.print("("); Serial.print(current_read); Serial.print(")  ");
   Serial.print("T_set: "); Serial.print(temp_set); Serial.print(" C  ");
   Serial.print("H: "); Serial.print(val);
-  Serial.print("\n");
+  
+  /************** display *****************/
+  String value_s=dtostrf(temp_set,3,0,buffor);
+  draw_value(value_s);
+  draw_power(val);
+  draw_ontarget(temp, temp_set);
+Serial.print("\n");
+delay(50);
 }
 
 /**************************************************************************************************/
 
 /************************ oled ***************************/
 void init_OLED(){
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  Serial.println("hello display");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false);  // initialize with the I2C addr 0x3C (for the 128x32)
   // Clear the display buffer.
   display.clearDisplay();
   display.setRotation(2);
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  display.setCursor(20,10);
   display.println("TS_T-12");
   display.display();
   //delay(interval);
   delay(1000);
-  float value=0.000;
+  //float value=0.000;
   //dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
-  String value_s=dtostrf(value,5,2,buffor);
-  value_s="-.--- ";
+  //String value_s=dtostrf(value,5,2,buffor);
+  //value_s="-.--- ";
   display.clearDisplay();
   //display_show(mode, value_s);
-	#endif
 }
-
+/*
 void display_show(int mode, String value_to_display){
   //oled_timed_out=0;
   //reset_timeout();
@@ -147,53 +155,43 @@ void draw_menu(int mode){ //mode menu constructor
     display.setTextColor(1,0);
     display.setTextSize(1);
     display.setCursor(0,0);
-    if (mode==1) {
+   
 		display.print("(D)");
-    }else{
-		display.print(" D ");
-    }
-    if (mode==2) {
-		display.print("(R)");
-    }else{
-		display.print(" R ");
-    }
-    if (mode==3) {
-		display.print("(V)");
-    }else{
-		display.print(" V ");
-    }
-    if (mode==4) {
-		display.print("(A)");
-    }else{
-		display.print(" A ");
-    }
-    if (mode==5) {
-		display.print("(C)");
-    }else{
-		display.print(" C ");
-    }
-    if (mode==6) {
-		display.print("(H)");
-    }else{
-		display.print(" H ");
-    }
-    if (mode==7) {
-		display.print("(T)");
-    }else{
-		display.print(" T ");
-    }
+       
     display.println("");
     display.display();
 }
-  
+  */
 void draw_value(String value_to_display){
-	display.setTextSize(3);
+	display.setTextSize(2.5);
     display.setCursor(20,11);
     display.setTextColor(1,0);
-    display.println(value_to_display);
+    display.println(value_to_display+ String(char(247)) + "C");
     display.display();
 }
-
+void draw_power(int power){
+  power=power/8;
+  display.fillRect(125,0,3,32-power, BLACK);
+  display.fillRect(125,32-power,3,power, WHITE);
+  display.display();
+}
+void draw_ontarget(int value, int target){
+  int target_center=75;
+  int overshoot = value - target;
+  //int spread=target/10;
+  int spread_a=min(-target/10*2*target_center/125,-1);
+  int spread_b=max(target/10*2*(125-target_center)/125,1);
+  //overshoot=constrain(overshoot, -spread*2*target_center/125, spread*2*(125-target_center)/125);
+  overshoot=constrain(overshoot, spread_a, spread_b);
+  int pos_a=min(target_center+overshoot*target_center/(-spread_a),target_center);
+  int pos_b=max(target_center+overshoot*target_center/(-spread_a),target_center);
+  display.fillRect(1,1,pos_a,2, BLACK);
+  display.fillRect(pos_a+1,1,pos_b,2, WHITE);
+  display.fillRect(pos_b+1,1,125,2, BLACK);
+  display.drawLine(target_center,0,target_center,4, WHITE);
+  display.display();
+}
+/*
 void oled_display_timeout(){
   unsigned long now=millis();
   byte delta;
@@ -211,4 +209,4 @@ void oled_display_timeout(){
       last_tick=now;
   	}
   }
-}
+}*/
